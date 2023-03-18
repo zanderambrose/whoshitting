632eba7aef2957636214a0f8579f91066810732c
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-18 17:50:48 -0400

    Get title from vanguard homepage.

--- main.py
+++ main.py
@@ -4,17 +4,29 @@ import requests
 
 class Venue:
     def __init__(self, name, url):
-        self.name = name
+        self.venue_name = name
         self.url = url
 
     def visit(self):
         try:
             page = requests.get(self.url)
-            return page.text
+            return page.content
         except:
-            print(f'Error visiting url for {self.name}')
+            print(f'Error visiting url for {self.venue_name}')
+
+    def make_soup(self):
+        content = self.visit()
+        return BeautifulSoup(content, 'html.parser')
+
+    def get_band_name(self):
+        soup = self.make_soup()
+        band_name = soup.find("h1")
+        print(f'BAND NAME: {band_name}')
+
+    def fetch(self):
+        self.get_band_name()
 
 
 if __name__ == "__main__":
     vangaurd = Venue("Village Vanguard", "https://villagevanguard.com/")
-    vangaurd.visit()
+    vangaurd.fetch()
