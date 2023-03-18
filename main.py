fa266719534b089873abaced88b39d058a7ce3b3
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-03-18 10:49:27 -0400

    Begin.

---
+++ main.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+
+
+class Venue:
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+    def visit():
+        pass
+
+
+if __name__ == "__main__":
+    print("hello world")
