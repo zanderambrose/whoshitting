bd412b95016ca36171639f05ee87ae5db00fd8c2
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-05-27 18:15:22 -0400

    Get smalls artists along with band leader.

--- venues.py
+++ venues.py
@@ -7,6 +7,8 @@ class Venue(ABC):
     def __init__(self, name, url):
         self.venue_name = name
         self.url = url
+        self.artists = []
+        self.make_soup()
 
     def visit(self):
         """
@@ -23,7 +25,7 @@ class Venue(ABC):
         Create Soup object for for further processing
         """
         content = self.visit()
-        return BeautifulSoup(content, 'html.parser')
+        self.soup = BeautifulSoup(content, 'html.parser')
 
     @abstractmethod
     def get_band_name(self):
@@ -32,37 +34,61 @@ class Venue(ABC):
         """
         pass
 
+    @abstractmethod
+    def get_artists(self):
+        """
+        Method to be implemented per Venue to return the sideman 
+        """
+        pass
+
     def run(self):
         """
         Method to be called for getting all important data
         """
         self.get_band_name()
+        self.get_artists()
         print(f'Band name for {self.venue_name}: {self.band_name}')
+        print(f'Sideman for {self.band_name}: {self.artists}')
 
 
 class Vanguard(Venue):
 
     def get_band_name(self):
-        soup = self.make_soup()
-        band_name = soup.find("h1")
+        band_name = self.soup.find("h1")
         self.band_name = band_name.text.strip()
         return super().get_band_name()
+
+    def get_artists(self):
+        pass
 
 
 class Smalls(Venue):
 
     def get_band_name(self):
-        soup = self.make_soup()
-        early_set_container = soup.find(
-            class_="event-display-today-and-tomorrow")
+        early_set_container = self.soup.find_all('article',
+                                                 class_="event-display-today-and-tomorrow")[1]
         early_set_data = early_set_container.find_all("a")
         self.band_name = early_set_data[0]["aria-label"].split(", ")[0]
         return super().get_band_name()
+
+    def get_artists(self):
+        early_set_container = self.soup.find_all('article',
+                                                 class_="event-display-today-and-tomorrow")[1]
+
+        artists = early_set_container.select("div > a")
+
+        for artist in artists:
+            self.artists.append(artist.text.strip())
+
+        return super().get_artists()
 
 
 class Mezzrow(Venue):
 
     def get_band_name(self):
+        pass
+
+    def get_artists(self):
         pass
 
 
@@ -71,10 +97,16 @@ class BlueNote(Venue):
     def get_band_name(self):
         pass
 
+    def get_artists(self):
+        pass
+
 
 class Smoke(Venue):
 
     def get_band_name(self):
+        pass
+
+    def get_artists(self):
         pass
 
 
@@ -83,8 +115,14 @@ class Birdland(Venue):
     def get_band_name(self):
         pass
 
+    def get_artists(self):
+        pass
+
 
 class Dizzys(Venue):
 
     def get_band_name(self):
+        pass
+
+    def get_artists(self):
         pass
