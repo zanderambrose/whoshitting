7b232bd9db527a2742a6b6ed62c7ef60b88541a2
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-05-29 17:45:18 -0400

    SmallsLive class to handle smalls and mezzrow.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 from src.vanguard import Vanguard
-from src.smalls import Smalls
-from src.mezzrow import Mezzrow
+from src.smallslive import SmallsLive
 from src.birdland import Birdland
 from src.bluenote import BlueNote
 from src.dizzys import Dizzys
@@ -9,8 +8,8 @@ from src.smoke import Smoke
 
 if __name__ == "__main__":
     vangaurd = Vanguard("Village Vanguard", "https://villagevanguard.com/")
-    smalls = Smalls("Smalls", "https://www.smallslive.com/")
-    mezzrow = Mezzrow("Mezzrow", "https://www.smallslive.com/")
+    smalls = SmallsLive("Smalls", "https://www.smallslive.com/")
+    mezzrow = SmallsLive("Mezzrow", "https://www.smallslive.com/")
     birdland = Birdland("Birdland", "https://birdlandjazz.com/")
     bluenote = BlueNote("Blue Note", "https://www.bluenotejazz.com/nyc/")
     dizzys = Dizzys("Dizzys", "https://2023.jazz.org/dizzys-club#upnext")
