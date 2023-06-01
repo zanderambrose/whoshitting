7cdc988cebbfa56ac88d9a4ce3e4b993862520be
Zander Ambrose <alexander.d.ambrose@gmail.com>
2023-06-01 19:51:52 -0400

    Add extract text helper function.

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -1,3 +1,6 @@
+import re
+
+
 def is_all_caps(string):
     # Check if the string is not empty and contains only alphabetic characters
     if string and string.isalpha():
@@ -14,3 +17,10 @@ def is_br_element(string):
         if string == "<br/>":
             return True
     return False
+
+
+def extract_text(string):
+    # Remove HTML tags using regular expressions
+    clean_string = re.sub('<.*?>', '', string)
+
+    return clean_string
